171d9486e66a2b5adcbbd84c1330c15b1a73f2c9
Dilip Mali <deepmali590@gmail.com>
2024-10-05 11:59:49 +0000

    container config done

---
+++ 01_basics/conversionopertion.js
@@ -0,0 +1,51 @@
+// let score =33  
+
+// console.log(typeof score);   number
+// console.log(typeof (score)); number (isko hum ese bhi likh skte hai)
+// but but but aage dekho 
+
+// let valueNumber = Number (score)
+
+// console.log(typeof valueNumber); Obviously ab iska datatypes  number me convert  ho gya kyoki hmne ab Number ko use kiya hali
+//  let score= Number ("33 abc")
+//  console.log(score);  ab k iska output hme (NaN) aaega kyoki score koi  purenumber nahi hai 
+
+
+// let score = Number (null)
+// console.log(score);  // iska output (0) aaega
+
+// let  score = Number (undefined)
+
+// console.log(score); ab iska output hme NAN aaega because ye koi number nahi hai || agar true hota (1) & agar false (0) return karta hai
+
+// "33" => 33
+// "33abc" => NaN  (typeof number)
+// true => 1; false 0;
+
+//  //
+
+// let isLoggedIn=1
+// let isLoggedIn=""
+// let isLoggedIn="Dilip sa"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+
+// 1. console.log(booleanIsLoggedIn); output=> true
+// 2. console.log(booleanIsLoggedIn); output=> false
+// 3. console.log(booleanIsLoggedIn); output=> true
+
+let someNumber=33
+
+let stringSomeNumber= String (someNumber)
+
+// console.log(stringSomeNumber);  output 33 but =>
+    // console.log(typeof stringSomeNumber); output  string kyoki ye number ab sting me convert ho chuka hai
+    
+
+
+
+
+
+ 
+
+
