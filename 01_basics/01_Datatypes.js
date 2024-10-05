42934595a5e5c77e37be99e7f82a53970388ba8e
Dilip Mali <deepmali590@gmail.com>
2024-10-05 11:31:34 +0000

    container config done

---
+++ 01_basics/01_Datatypes.js
@@ -0,0 +1,41 @@
+"use strict"; //treat all js code as newer java script version
+
+// alert(2+3)  // we are usong node js  ,not browser  so yha terminal aapko alert function nahi chalta hai ye only browser read karta hai
+
+// console.log(3+3);  ye code hamara excute ho jaega  but agar hum ese likhe ==>
+// console.log(3+3) console.log("dilip"); ye exicute nahi hoga kyoki hmne first consloe ke bad hmne (;) nahi lagaya
+
+// wese Javascript bina (;) bina hi code ko read kar deta hai but kuch chizee fix hoti hai jisme hum isko use nahi kar skte
+
+// code ko hme sahi tarike se bhi likhna aana chahiye
+
+
+
+//Data Types??
+ 
+let name = 'Dilip'
+let age=23
+let accountLogIn=true/false
+
+// ? wese kitne types ke datatypes hote hai basically chalo dekhte hai//
+// all  Primitive datatypes of javascript name ==> 
+     
+//  number => 2 to power 53
+//  bigint  => big number 
+//  string  => "", ''
+//  boolean => true/false
+//  null  => standalone value  uski velue empty hai
+//  undefined  =>  kisi verebail ko hu defined nahi karte hai jese  let name; kuch ese 
+//  symbol =>unquie
+
+// object
+
+// console.log(typeof "Dilip");  typeof string
+// console.log(typeof age(upar likha hua hai));  type of number 
+// console.log(typeof null); typeof object
+// console.log(typeof undefined);  typeof is undefined
+
+  
+
+
+
--- 01_basics/01_let,const,var.js
+++ 01_basics/01_let,const,var.js
@@ -1,1 +1,35 @@
-console.log("hello b rother")
+// console.log("hello b rother")
+
+const accountID=144563
+let accountEmail="deep9828@gmail.com"
+var accountpassword="12345"
+
+// accountID=22 //Not Allowed 
+accountEmail="malidep9828@gmail.com"
+accountpassword="15243376"
+accountCity="palli"
+
+
+// let accountNAme;   (undefined kyoki isko hmnne kahi defeined .nahi kiya hai)
+console.table([accountID, accountEmail, accountpassword ,accountCity])
+
+
+//const ko app bar bar Declared. Nahi kar Sakte.
+//var jo hai bar bar redeclared ho jata tha isl0o iskko jitni bar ek hiname se verebails ko likhte hai wo apne apa ko chanhege kar deta tha 
+// isliye javascirpt ne ek new verebails ko launch wo tha "let" 
+
+// { isko hum scope kahte hai}
+
+/*
+ var= "preferr not to use var because of issue in block scope and functional scope"
+*/
+
+
+
+
+
+
+// console.log(accountID);
+// console.log(accountEmail);
+// console.log(accountpassword);  new trick of console your input  console.table("")
+// console.log(accountCity);
