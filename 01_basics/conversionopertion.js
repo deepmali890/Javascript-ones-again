cc8752a8bcfb3bebba9fc6a70753a434fec80339
Dilip Mali <deepmali590@gmail.com>
2024-10-05 16:06:13 +0000

    container config done

--- 01_basics/conversionopertion.js
+++ 01_basics/conversionopertion.js
@@ -28,7 +28,7 @@
 // let isLoggedIn=""
 // let isLoggedIn="Dilip sa"
 
-let booleanIsLoggedIn= Boolean(isLoggedIn)
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
 
 // 1. console.log(booleanIsLoggedIn); output=> true
 // 2. console.log(booleanIsLoggedIn); output=> false
@@ -41,6 +41,81 @@ let stringSomeNumber= String (someNumber)
 // console.log(stringSomeNumber);  output 33 but =>
     // console.log(typeof stringSomeNumber); output  string kyoki ye number ab sting me convert ho chuka hai
     
+
+
+
+//********************************** Opertions *********************//   
+
+let values=3
+let negValues= -values
+
+// console.log(negValues);   iska answer aapko negitive hi milega Obviously (-3) milega
+
+// console.log(2+2);  // 4
+// console.log(2-2);  // 0
+// console.log(2*2);  //4
+// console.log(2**2); //4
+// console.log(2/2);  //0
+// console.log(2%2);  // 0
+
+
+//*****//
+//  let str1="Dilip"
+//  let str2=" Mali"
+
+//  let str3=str1 + str2
+
+//  console.log(str3);  output is Dilip MAli
+
+// but but but//
+//  console.log(1 + "1");  output is not 2  || output is 1 1 beacuse dono number nahi hai ek number hai or ek string formet me hai kabhi bhi hamesha number (+) hote hai 
+
+// lekin confusion  yhan aata hai
+
+//  console.log("1" +2 +2);  answer  is 122 esa kyu beacuse yha straing me hi aaone isko string ke sath  joda hai
+// console.log(1+2+"4"); answer is 34 kyoki aapne phle isko number ke sath joda hai fir aapne isko string ke sath joda hai
+
+
+// console.log(3+4*5%2); anmswer is 3 but ye real life me likhne ka tarika nahi hai
+
+// console.log((3+4) *5%2);  answer is 1
+
+// console.log(+true);  aswer is 1 beceause by default  true ki value 1 hii hoti hai
+
+// console.log(+ ""); anser is 0 becuse by default value iski 0 hai
+
+
+// let gameCounter=100
+// gameCounter++;
+//gameCounte=+1
+// ++gameCounter  output is same
+// console.log(gameCounter); answer is 101 kuoki (++) 1 ka increment  hota hai or vahi agar hum (--) ka use karte hai to answeris 99
+
+
+//*****************//
+
+// console.log(2>1);  true
+// console.log(2<1);  false 
+// console.log(2>=1);  true
+// console.log(2==1);  false
+// console.log(2 !=1);  true
+
+// console.log(null >0);  false
+// console.log(null ==0); false
+// console.log(null >=0); true
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+
 
 
 
---
+++ 01_basics/datatypes-summry.js
@@ -0,0 +1,35 @@
+// # Primitive => 7 types: String, Number, Boolearn, null, undefined, symbol, BigInt
+
+// # Reference (Non Primitive Data Types)  
+// Array, Objects, Functions 
+
+// const score=100  number
+// const value=100.3 is an number
+// const isLogedIn=false bOolean
+// let oneTamp=null Null
+// let userEmail; Undefined
+// let id=Symbol('123')
+// let anthorId=Symbol('123')
+
+// console.log(id===anthorId);  false  symbol
+
+// ?? Arrays//
+// const heros=["shaktiman", "nagraj","doga"]   arrays
+
+// ? objects//
+
+
+//  let myObj={
+//     name:"dilip",
+//     age:22,
+//     pass:true,
+// }  is a Object  {}=>iske andar jo bhi values hai wo sabhi object me hai
+
+
+// function
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+
